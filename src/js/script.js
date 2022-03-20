390f99b9f380dc9e7a928134471b9567e9b629d4
Willbeckh <wileyhndirangu04@gmail.com>
2022-03-20 19:25:24 +0300

    ft: update order checkout logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,39 @@
 let numberOfPizzas = parseInt($(".output").text());
+let orderOutput = $(".feedback-text"); // holds output of pizza order
 
 $().ready(function () {
   console.log("page loaded!");
 
-  validateForm();
-  
+  formData();
   addPizzaAmount();
 });
 
-// form validate
-const validateForm = () => {
+// form object for data validation
+const formData = () => {
   $(".pizza-form").submit(function (submit) {
     submit.preventDefault();
-    const size = $("input[name=size]:checked").val();
-    const topps = $("input[name=topps]:checked").val();
-    const crust = $(".crust").val();
+    const size = Number($("input[name=size]:checked").val());
+    const topps = Number($("input[name=topps]:checked").val());
+    const crust = Number($(".crust").val());
+    let totalCost = numberOfPizzas * (size + topps + crust);
 
-    let order = new Pizza(size, topps, crust, numberOfPizzas);
+    //for getting text content only.
+    const sizeChoice = $("input[name=size]:checked + label").text();
+    const crustChoice = $(".crust option:selected").text();
+    const toppingsChoice = $("input[name=topps]:checked + label").text();
+
+    let order = new Pizza(
+      sizeChoice,
+      toppingsChoice,
+      crustChoice,
+      numberOfPizzas,
+      totalCost
+    );
     console.log(order.getPizzaDetails());
-    // $(".feedback-text").text(order.getPizzaDetails());
+    orderOutput.text(order.getPizzaDetails());
+    orderOutput.text(order.orderCheckout());
 
-    // add checkout show code
-    // return this.reset();
+    $(".checkout-order").show();
   });
 };
 
@@ -35,22 +47,40 @@ class Pizza {
     this.grandTotal = grandTotal;
   }
   getPizzaDetails() {
-    return `You ordered ${this.orders} pizza(s) of size: ${this.sizeOfPizza}, toppings : ${this.toppings} & pizza crust: ${this.pizzaCrust}`;
+    return `You ordered ${this.orders}, ${this.sizeOfPizza} pizza(s), toppings : ${this.toppings} & pizza crust: ${this.pizzaCrust}: total cost ${this.grandTotal}`;
   }
 
-  // try calculate the total
-  // try use a prototype for
+  // checkout order
+  orderCheckout() {
+    formData();
+    $(".checkout-order").click(function () {
+      // orderOutput.text(`Total cost is: ${totalCost}`);
+      // $(".order-btn").hide();
+
+      $(".deliver-option").show(function () {
+        $(".btn.yes").click(() => {
+          $(".location-input").css("display", "block");
+          return alert("your order will be delivered in a few");
+        });
+
+        $(".btn.no").click(() => {
+          $(".deliver-option").css("display", "none");
+          return alert("Thank you!, enjoy your meal");
+        });
+      });
+    });
+  }
 }
 
+// calculate number of pizzas for order
 let addPizzaAmount = () => {
-  $(".add-pizza").click(function () {
+  $(".add").click(function () {
     // handle edge-cases
     if (numberOfPizzas === 10)
       return alert("cannot place order for more than 10 pizzas at a go");
     $(".output").text(++numberOfPizzas);
   });
-
-  $(".deduct-pizza").click(function () {
+  $(".subtract").click(function () {
     if (numberOfPizzas === 1) return alert("number of pizzas must be 1");
     $(".output").text(--numberOfPizzas);
   });
